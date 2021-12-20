90692aaeaf49e475361fce6449b619b9af6ec067
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-20 19:59:54 +0200

    Added files

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="texture">
+      <header id="home" className="page">
+        <div className="navigation single-page-nav">
+          <div className="container_12">
+            <div className="grid_12">
+              <h1 className="logo">
+                <a href="index.html">N. Nikolaev</a>
+              </h1>
+              <nav>
+                <ul>
+                  <li><a href="#home" className="current">Home</a></li>
+                  <li><a href="#portfolio">Portfolio</a></li>
+                  <li><a href="#skills">Skills</a></li>
+                  <li><a href="#experience">Experience</a></li>
+                  <li><a href="#testimonials">Testimonials</a></li>
+                  <li><a href="#contacts">Contacts</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div id="camera_wrap">
+          <div data-src="images/slide-1.jpg">
+            <div className="caption fadeIn">Your Photographer <span className="bull"></span> Coder  <span className="bull"></span> Designer</div>
+          </div>
+          <div data-src="images/slide-2.jpg">
+            <div className="caption fadeIn">Impressive Designs, Suitable for Cool Projects</div>
+          </div>
+          <div data-src="images/slide-3.jpg">
+            <div className="caption fadeIn">Designs that Work for Your Benefits</div>
+          </div>
+        </div>
       </header>
     </div>
   );
