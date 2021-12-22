a6e7fec579026bbfbc9a4e68c5366f9da6c61af5
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-22 09:26:34 +0200

    Create Testimonials

---
+++ client/src/components/Testimonials/Testimonials.js
@@ -0,0 +1,43 @@
+
+const Testimonials = () => {
+
+    return (
+        <section id="testimonials" class="page">
+            <div class="container_12">
+                <div class="grid_12">
+                    <h2 class="color2">Testimonials</h2>
+                </div>
+                <div class="grid_4">
+                    <blockquote class="bq1">
+                        <img src="images/page-img_2.jpg" alt="" />
+                        <div class="extra_wrapper">
+                            <div class="text-2"><a href="#">Helen Thomson</a></div>
+                            Korem ipsum dolor sittur dipiscing elit. In molliserat mattis neque facilisisltries wertolio dasererat
+                        </div>
+                    </blockquote>
+                </div>
+                <div class="grid_4">
+                    <blockquote class="bq1">
+                        <img src="images/page-img_3.jpg" alt="" />
+                        <div class="extra_wrapper">
+                            <div class="text-2"><a href="#">Mark Grey</a></div>
+                            Morem ipsum dolor sittur dipiscing elit. In molliserat mattis neque facilisisltries wertolio daserera
+                        </div>
+                    </blockquote>
+                </div>
+                <div class="grid_4">
+                    <blockquote class="bq1">
+                        <img src="images/page-img_4.jpg" alt="" />
+                        <div class="extra_wrapper">
+                            <div class="text-2"><a href="#">Irma Wood</a></div>
+                            Torem ipsum dolor sittur dipiscing elit. In molliserat mattis neque facilisisltries wertolio dasererum
+                        </div>
+                    </blockquote>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Testimonials;
