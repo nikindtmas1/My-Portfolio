bcfe187283d76be4c0297fc690600b3cb8b35933
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-22 13:40:17 +0200

    Added images

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -37,28 +37,37 @@ const Skills = () => {
                 </div>
                 <div className="grid_4">
                     <blockquote className="bq1">
-                        <img src="images/page-img_41.jpg" alt="" />
+                        <img src="images/page-img_51.jpg" alt="" />
                         <div className="extra_wrapper">
                             <div className="text-2"><a href="#">React Js</a></div>
-                            JavaScript is a programming language commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites.
+                            React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.
                         </div>
                     </blockquote>
                 </div>
                 <div className="grid_4">
                     <blockquote className="bq1">
-                        <img src="images/page-img_41.jpg" alt="" />
+                        <img src="images/page-img_61.jpg" alt="" />
                         <div className="extra_wrapper">
                             <div className="text-2"><a href="#">Express Js</a></div>
-                            JavaScript is a programming language commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites.
+                            Express.js, or simply Express, is a back end web application framework for Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs.
                         </div>
                     </blockquote>
                 </div>
                 <div className="grid_4">
                     <blockquote className="bq1">
-                        <img src="images/page-img_41.jpg" alt="" />
+                        <img src="images/page-img_71.jpg" alt="" />
                         <div className="extra_wrapper">
                             <div className="text-2"><a href="#">Node.Js</a></div>
-                            JavaScript is a programming language commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites.
+                            Node. js (Node) is an open source development platform for executing JavaScript code server-side. Node is useful for developing applications that require a persistent connection from the browser to the server and is often used for real-time applications such as chat, news feeds and web push notifications.
+                        </div>
+                    </blockquote>
+                </div>
+                <div className="grid_4">
+                    <blockquote className="bq1">
+                        <img src="images/page-img_81.jpg" alt="" />
+                        <div className="extra_wrapper">
+                            <div className="text-2"><a href="#">MongoDB</a></div>
+                            MongoDB is an open source NoSQL database management program. NoSQL is used as an alternative to traditional relational databases. NoSQL databases are quite useful for working with large sets of distributed data. MongoDB is a tool that can manage document-oriented information, store or retrieve information.
                         </div>
                     </blockquote>
                 </div>
