8f610610776395a7a357e1c348211bbfc6d32ffa
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-22 09:25:25 +0200

    Create Experiences

---
+++ client/src/components/Experience/Experience.js
@@ -0,0 +1,56 @@
+
+const Experience = () => {
+
+    return (
+        <section id="experience" class="page">
+            <div class="container_12">
+                <div class="grid_12">
+                    <h2>Work Experience</h2>
+                    <div class="block-1">
+                        <div class="grid_4 alpha">
+                            <div class="block-1__left">
+                                <h3>2012 - Present</h3>
+                                <time datetime="2014-01-01" class="text-2">March</time>
+                                <div class="ta__right">
+                                    <div class="text-2 color1">Design Studio</div>
+                                    Lorem ipsum dolor sit tetur dipiscing elit. In mollis erat mattis neque facilisis.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid_8 omega">
+                            <div class="block-2__right">
+                                <h3>Job Description</h3>
+                                <div class="text-2">Graphic Designer</div>
+                                <p>Dorem ipsum dolor sit tetur dipiscing elit. In mollis erat mattis neque facilisis, situltries wertolio dasererat rutrum. Lorem ipsum dolor sit tetur dipiscing elit.  </p>
+                                Oliquam nibh ante, egestas id dictum a, commodo luctus libero. Praesent faucibus malesuadar faucibusonec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elitllam consectetur orci sed nulla facilisis consequaturabitur vel lorem sit
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block-1">
+                        <div class="grid_4 alpha">
+                            <div class="block-1__left">
+                                <h3>2007 - 2012</h3>
+                                <time datetime="2014-01-01" class="text-2">April <span>August</span></time>
+                                <div class="ta__right">
+                                    <div class="text-2 color1">Freelancer</div>
+                                    Lorem ipsum dolor sit tetur dipiscing elit. In mollis erat mattis neque facilisis.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid_8 omega">
+                            <div class="block-2__right">
+                                <h3>Job Description</h3>
+                                <div class="text-2">Graphic Designer</div>
+                                <p>Lorem ipsum dolor sit tetur dipiscing elit. In mollis erat mattis neque facilisis, situltries wertolio dasererat rutrum. Lorem ipsum dolor sit tetur dipiscing elit.  </p>
+                                Aliquam nibh ante, egestas id dictum a, commodo luctus libero. Praesent faucibus malesuadar faucibusonec laoreet metus id laoreet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elitllam consectetur orci sed nulla facilisis consequaturabitur vel lorem sit
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Experience;
