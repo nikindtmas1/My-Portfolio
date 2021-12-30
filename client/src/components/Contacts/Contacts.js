b83f089ae22fecf074a78f0007adf9ac58f814ef
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-30 14:19:28 +0200

    Change city

--- client/src/components/Contacts/Contacts.js
+++ client/src/components/Contacts/Contacts.js
@@ -5,101 +5,60 @@ import emailjs from 'emailjs-com';
 
 const Contacts = () => {
     const history = useHistory();
-   
+
     const form = useRef();
 
     const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_h9aidmw', 'template_j6a3t4s', form.current, 'user_eSMFjCjC1C4AXtnDjwkCx')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        })
-        .then(alert('Your message has been sent! We will be in touch soon.'))
-        .then(history.push('/home'));
+        e.preventDefault();
+
+        emailjs.sendForm('service_h9aidmw', 'template_j6a3t4s', form.current, 'user_eSMFjCjC1C4AXtnDjwkCx')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+            .then(alert('Your message has been sent! We will be in touch soon.'))
+            .then(history.push('/home'));
     };
 
     return (
         <section id="contacts" className="page">
             <div className="container_12">
                 <div className="grid_12">
-                        <br />
-                        <br />
-                        <br />
-                        <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
                     <h2>Get in Touch</h2>
                 </div>
                 <div className="grid_6">
-                    {/* <form id="contact-form" onSubmit={sendEmail} >
+
+                    <form id="contact-form" ref={form} onSubmit={sendEmail}>
                         <div className="contact-form-loader"></div>
                         <fieldset>
                             <label className="name">
-                                <input type="text" name="user_name" placeholder="Name*:"  data-constraints="@Required @JustLetters"  />
-                                <span className="empty-message">*This field is required.</span>
-                                <span className="error-message">*This is not a valid name.</span>
+                                <input type="text" placeholder="Name*:" name="user_name" />
                             </label>
                             <label className="email">
-                                <input type="text" name="user_email" placeholder="E-mail*:"  data-constraints="@Required @Email" />
-                                <span className="empty-message">*This field is required.</span>
-                                <span className="error-message">*This is not a valid email.</span>
-                            </label>
-                            <label className="phone">
-                                <input type="text" name="user_phone" placeholder="Telephone:"  data-constraints="@Required @JustNumbers" />
-                                <span className="empty-message">*This field is required.</span>
-                                <span className="error-message">*This is not a valid phone.</span>
+                                <input type="text" placeholder="E-mail*:" name="user_email" />
                             </label>
                             <label className="message">
-                                <textarea name="message" placeholder="Message*:" data-constraints='@Required @Length(min=20,max=999999)' />
-                                <span className="empty-message">*This field is required.</span>
-                                <span className="error-message">*The message is too short.</span>
+                                <textarea placeholder="Message*:" name="message" />
                             </label>
                             <div>
-                                {/* <input className="btn" type="submit" value="Send" data-type="submit" /> 
-                                <a href="" value="Send" className="btn" data-type="submit">submit </a>
+                                <input className="btn" type="submit" value="Send" />
                             </div>
-                        </fieldset>
-                        <div className="modal fade response-message">
-                            <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                        <h4 className="modal-title">Modal title</h4>
-                                    </div>
-                                    <div className="modal-body">
-                                        You message has been sent! We will be in touch soon.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form> */}
-<form id="contact-form" ref={form} onSubmit={sendEmail}>
-<div className="contact-form-loader"></div>
-<fieldset>
-      <label className="name">
-      <input type="text" placeholder="Name*:" name="user_name" />
-      </label>
-      <label className="email">
-      <input type="text" placeholder="E-mail*:" name="user_email" />
-      </label>
-      <label className="message">
-      <textarea placeholder="Message*:" name="message" />
-      </label>
-      <div>
-      <input className="btn" type="submit" value="Send" />
-      </div>
 
-      
-    </fieldset>
-    </form>
+
+                        </fieldset>
+                    </form>
                 </div>
                 <div className="grid_6">
                     {/* <p>24/7 support is on for all <a href="http://www.templatemonster.com/" rel="nofollow" className="color1">premium templates</a>.</p>
                     <p><a href="http://www.templatetuning.com/" rel="nofollow" className="color1">TemplateTuning</a> will help you with customization of the chosen <br /> theme.</p> */}
                  
                     9000 Postcode,<br />
-                    Varna,  43.217 / 27.917<br />
+                    City: Varna<br />
                     Telephone: +359 878 948 454<br />
                     E-mail: <a href="https://google.com">nikindtmas1@gmail.com</a>
                 </div>
