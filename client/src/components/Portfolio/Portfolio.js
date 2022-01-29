9fbf4d86710ec7f7c488a2ec457f4b6836e0fa1a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-01-29 19:06:21 +0200

    Make change

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -11,13 +11,13 @@ const Portfolio = () => {
                         <br />
                         <h2 className="color2">My Portfolio</h2>
                         <div id="filters" className="button-group">
-                            <a href="https://github.com/nikindtmas1" className="btn is-checked" data-filter="*">Dashboard</a>
+                            {/* <a href="https://github.com/nikindtmas1" className="btn is-checked" data-filter="*">Dashboard</a> */}
                             <a href="https://github.com/nikindtmas1/My-Spa-Menu" className="btn" data-filter=".ill">Front End</a>
                             <a href="https://github.com/nikindtmas1/My-Cubic-MongoDb" className="btn" data-filter=".photo">Back End</a>
                             <a href="https://github.com/nikindtmas1/Journey-React-Project" className="btn" data-filter=".design">React App</a>
-                            <a href="https://react-project-journey.web.app/" className="btn" data-filter=".design">React Web App</a>
-                            <a href="https://my-ecommerce-ee27f.web.app/" className="btn" data-filter=".design">my-eCommerce</a>
-                            <a href="https://rirst-typescript-app.web.app/" className="btn" data-filter=".design">Typescript-app</a>
+                            <a href="https://react-project-journey.web.app/" className="btn" data-filter=".design">React Web</a>
+                            <a href="https://my-ecommerce-ee27f.web.app/" className="btn" data-filter=".design">eCommerce</a>
+                            <a href="https://rirst-typescript-app.web.app/" className="btn" data-filter=".design">TypeScript</a>
                         </div>
                         <div className="el gallery" id="isotope">
                             <div className="element design">
